020f0a8e1b67ae8a17de2f4c8f6c52758d0ac664
JAYJAY <2422301985@qq.com>
2025-05-20 15:59:45 +0800

    test_basic_type.py初步完成

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## Introduce
 
-本项目测试Linux、mac与win主流操作系统下的Python3.8、3.9、3.10、3.11、3.12版本的pickle模块的序列化与反序列化功能。  
+本项目测试Linux、mac与win主流操作系统下的Python3.8、3.9、3.10、3.11、3.12版本的pickle模块的序列化功能。  
 本项目使用conda环境进行不同版本下的管理测试。
 
 ## Project Structure
--- black_test/test_basic_type.py
+++ black_test/test_basic_type.py
@@ -1,9 +1,27 @@
 import unittest
+import pickle
+import hashlib
+import random
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+class TestBasicType(unittest.TestCase):
+    def test_int(self):
+        a = 1
+        a_pickle1 = pickle.dumps(a)
+        a_pickle2 = pickle.dumps(a)
+        assert (hashlib.sha256(a_pickle1).hexdigest() == hashlib.sha256(a_pickle2).hexdigest())
+
+    def test_float(self):
+        a = 1.0
+        a_pickle1 = pickle.dumps(a)
+        a_pickle2 = pickle.dumps(a)
+        assert (hashlib.sha256(a_pickle1).hexdigest() == hashlib.sha256(a_pickle2).hexdigest())
+
+    def test_high_precision_float(self):
+        a = random.uniform(1.0, 1000000000.0)
+        a_pickle1 = pickle.dumps(a)
+        a_pickle2 = pickle.dumps(a)
+        assert (hashlib.sha256(a_pickle1).hexdigest() == hashlib.sha256(a_pickle2).hexdigest())
 
 
 if __name__ == '__main__':
